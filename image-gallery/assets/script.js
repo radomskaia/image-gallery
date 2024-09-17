3539b7cd1fa759417b26628bce570a2780aaed68
Alena Radomskaia <radomskaia.a.i@gmail.com>
2024-09-18 01:57:07 +0700

    refactor: make infinity scroll

--- image-gallery/assets/script.js
+++ image-gallery/assets/script.js
@@ -1,34 +1,47 @@
 const URL = "https://api.unsplash.com";
 const ACCESS_KEY = 'wSEvY-mEibQueTI6OUJSEt3kWPxmWbTC8wFcO2Nt1UM';
 const TYPE_OF_IMAGE = "photos";
-const DESCTOP_SCREEN_WIDTH = 704;
-const TABLET_SCREEN_WIDTH = 704;
+const DESKTOP_SCREEN_WIDTH = 1216;
+const TABLET_L_SCREEN_WIDTH = 928;
+const TABLET_S_SCREEN_WIDTH = 704;
 const PHONE_SCREEN_WIDTH = 480;
 
 const imagesBox = document.querySelector(".grid");
 const searchBtn = document.querySelector(".search-btn");
 const searchInput = document.querySelector(".search-input");
-let pageNumber = 1;
+const mediaQueryListDesktop = window.matchMedia(`(max-width: ${DESKTOP_SCREEN_WIDTH}px)`);
+const mediaQueryListTabletL = window.matchMedia(`(max-width: ${TABLET_L_SCREEN_WIDTH}px)`);
+const mediaQueryListTabletS = window.matchMedia(`(max-width: ${TABLET_S_SCREEN_WIDTH}px)`);
+const mediaQueryListPhone = window.matchMedia(`(max-width: ${PHONE_SCREEN_WIDTH}px)`);
 
-async function getImagesData(searchValue = '') {
-    let fetchURL;
-    let result;
+let pageNumber = 0;
+let imagesPerPage = 30;
+let isLoad, isSearch;
+
+async function getData() {
+    let searchValue = searchInput.value;
+    pageNumber++;
+    let fetchURL, result, imagesData;
     if (!searchValue) {
-        console.log('not search')
-        fetchURL = `${URL}/${TYPE_OF_IMAGE}/?&page=1&per_page=30&client_id=${ACCESS_KEY}`;
+        fetchURL = `${URL}/${TYPE_OF_IMAGE}/?&page=${pageNumber}&per_page=${imagesPerPage}&client_id=${ACCESS_KEY}`;
     } else {
-        console.log('search')
-        fetchURL = `${URL}/search/${TYPE_OF_IMAGE}/?&query='${searchValue}'&page=1&per_page=30&client_id=${ACCESS_KEY}`;
+        fetchURL = `${URL}/search/${TYPE_OF_IMAGE}/?&query='${searchValue}'&page=${pageNumber}&per_page=${imagesPerPage}&client_id=${ACCESS_KEY}`;
     }
 
-    result = await fetch(fetchURL);
-    const imagesData = await result.json();
-    createImg(searchValue ? imagesData.results : imagesData)
-    pageNumber++
+    try {
+        result = await fetch(fetchURL);
+        imagesData = await result.json();
+    } catch (error) {
+        console.log(error);
+    }
+
+    return imagesData;
 }
 
-function createImg(imagesData) {
-    imagesData.forEach(imagesObject => {
+
+async function renderData() {
+    const imagesData = await getData();
+    (searchInput.value ? imagesData.results : imagesData).forEach(imagesObject => {
         const imgWrapper = document.createElement("div");
         imgWrapper.classList.add("img-wrapper");
         imagesBox.appendChild(imgWrapper);
@@ -40,58 +53,68 @@ function createImg(imagesData) {
         imgWrapper.style.gridRow = `span ${span}`;
         imgItem.setAttribute("data_id", imagesObject.id);
         imgWrapper.appendChild(imgItem);
-
     })
 }
+
+
+async function searchQuery() {
+    if (isSearch) {
+        searchInput.value = '';
+        searchBtn.src = "assets/img/icons/icons8-search-48.png";
+        isSearch = false;
+    } else {
+        imagesBox.innerHTML = '';
+        pageNumber = 0;
+        await renderData()
+        searchBtn.src = "assets/img/icons/icons8-close-50.png";
+        isSearch = true;
+    }
+}
+
+function changeGridColumns() {
+    switch (true) {
+        case mediaQueryListPhone.matches:
+            imagesBox.style.gridTemplateColumns = "auto";
+            break;
+        case mediaQueryListTabletS.matches:
+            imagesBox.style.gridTemplateColumns = "auto auto";
+            break;
+        case mediaQueryListTabletL.matches:
+            imagesBox.style.gridTemplateColumns = "auto auto auto";
+            break;
+        case mediaQueryListDesktop.matches:
+            imagesBox.style.gridTemplateColumns = "auto auto auto auto";
+            break;
+        default:
+            imagesBox.style.gridTemplateColumns = "auto auto auto auto auto";
+    }
+}
+
+async function getNewPage() {
+    const heightForRequest = (imagesBox.scrollHeight - document.documentElement.clientHeight);
+    if (!isLoad && scrollY >= heightForRequest) {
+        isLoad = true
+
+        await renderData()
+        pageNumber++;
+        isLoad = false;
+    }
+}
+
+
+
+await renderData()
+changeGridColumns()
 
 searchBtn.addEventListener("click", searchQuery);
 document.addEventListener("keydown", (e) => {
     if (e.key === "Enter") searchQuery()
 })
+mediaQueryListDesktop.addEventListener("change", changeGridColumns);
+mediaQueryListTabletL.addEventListener('change', changeGridColumns)
+mediaQueryListTabletS.addEventListener('change', changeGridColumns)
+mediaQueryListPhone.addEventListener('change', changeGridColumns)
+window.addEventListener('scroll', getNewPage);
 
-function searchQuery() {
-    imagesBox.innerHTML = ''
-    getImagesData(searchInput.value)
-    console.log(searchInput.value)
-    searchBtn.src = "assets/img/icons/icons8-close-50.png";
-    searchBtn.removeEventListener("click", searchQuery);
-    searchBtn.addEventListener("click", cleanQuery);
-}
 
-function cleanQuery() {
-    searchInput.value = '';
-    searchBtn.src = "assets/img/icons/icons8-search-48.png";
-    searchBtn.removeEventListener("click", cleanQuery);
-    searchBtn.addEventListener("click", searchQuery);
-}
 
-getImagesData();
-
-const mediaQueryListTablet = window.matchMedia(`(max-width: ${TABLET_SCREEN_WIDTH}px)`);
-const mediaQueryListPhone = window.matchMedia(`(max-width: ${PHONE_SCREEN_WIDTH}px)`);
-
-switch (true) {
-    case mediaQueryListPhone.matches :
-        imagesBox.style.gridTemplateColumns = "auto";
-        break;
-    case mediaQueryListTablet.matches :
-        imagesBox.style.gridTemplateColumns = "auto auto";
-        break;
-    default:
-        imagesBox.style.gridTemplateColumns = "auto auto auto auto auto";
-}
-
-function changesForTabletWidth() {
-    if (mediaQueryListTablet.matches) {
-        imagesBox.style.gridTemplateColumns = "auto auto";
-    } else {
-        imagesBox.style.gridTemplateColumns = "auto auto auto";
-    }
-}
-
-function changesForPhoneWidth() {
-    imagesBox.style.gridTemplateColumns = mediaQueryListPhone.matches ? "auto" : "auto auto";
-}
-
-mediaQueryListTablet.addEventListener('change', changesForTabletWidth)
-mediaQueryListPhone.addEventListener('change', changesForPhoneWidth)
